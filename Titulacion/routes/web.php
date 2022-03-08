d9cde1e91ab1bf0be10f34f7c1f801a27ca5cdaf
jacksonp12 <jacksondemente@gmail.com>
2022-03-07 21:03:11 -0500

    Formulario proyecto empresarial

--- Titulacion/routes/web.php
+++ Titulacion/routes/web.php
@@ -19,4 +19,5 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::resource('empresariales', App\Http\Controllers\EmpresarialeController::class);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
